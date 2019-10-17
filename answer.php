48dcf4cf479f30e7b327ef9a15889ad38b559f4c
Shinjia <shinjia168@gmail.com>
2019-10-17 20:08:59 +0800

    update

---
+++ answer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>九九乘法CAI練習</title>
+</head>
+<body>
+<h1>九九乘法CAI練習</h1>
+<p><img src="images/2.jpg"><img src="images/1.jpg"></p>
+
+</body>
+</html>
+
---
+++ question.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>九九乘法CAI練習</title>
+</head>
+<body>
+<h1>九九乘法CAI練習</h1>
+<p><img src="images/3.jpg"><img src="images/mul.jpg"><img src="images/7.jpg"></p>
+
+<p><a href="answer.php">看答案</a></p>
+</body>
+</html>
+
